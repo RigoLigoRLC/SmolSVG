f4b056112fdb0c571e18bfe7e2e92bc06899f585
RigoLigoRLC <rigoligo03@gmail.com>
2020-09-05 00:27:52 +0800

    Fix crash issue and switched to chained list for path storage

--- main.cpp
+++ main.cpp
@@ -7,7 +7,24 @@ int main()
 
   std::getline(std::cin, a);
 
-  SmolSVG::readPathString(a);
+  auto path = SmolSVG::readPathString(a);
+
+  for(auto &i : path)
+    switch(i->type())
+    {
+      case SmolSVG::commandType::LineTo:
+        std::cout << "Line object" << std::endl;
+        break;
+      case SmolSVG::commandType::CurveTo:
+        std::cout << "Cubic Curve object" << std::endl;
+        break;
+      case SmolSVG::commandType::QuadTo:
+        std::cout << "Quadratic Curve  object" << std::endl;
+        break;
+      case SmolSVG::commandType::ArcTo:
+        std::cout << "Arc object" << std::endl;
+        break;
+    }
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
